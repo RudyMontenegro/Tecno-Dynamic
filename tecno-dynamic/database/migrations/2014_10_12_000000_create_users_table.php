6b3da0189077edee35d6c0db05cbc15184a81cce
alex-rodrigo-64 <alexcalle64@gmail.com>
2021-06-30 15:13:27 -0400

    sucursal

--- tecno-dynamic/database/migrations/2014_10_12_000000_create_users_table.php
+++ tecno-dynamic/database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,13 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
+            $table->string('cedula');
+            $table->string('direccion');
+            $table->string('telefono');
+            $table->string('fecha_ingreso');
+            $table->string('puesto');
+            $table->string('nivel_usuario');
+            $table->string('usuario');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
