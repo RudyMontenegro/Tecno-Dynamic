2c392990b27e6f67dcb13cf193612b143e87d7df
kaolio <milanando300@gmail.com>
2021-06-30 17:25:22 -0400

    sucursal2.1

--- tecno-dynamic/resources/views/sucursal/index.blade.php
+++ tecno-dynamic/resources/views/sucursal/index.blade.php
@@ -5,10 +5,7 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                
-            </div>
-        </div>
+        
     </div>
 </div>
 @endsection
