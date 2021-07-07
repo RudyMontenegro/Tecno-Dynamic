8921a42480d8966912f1dc7182db46112f25dc96
rolidoss <rudymontenegro4@gmail.com>
2021-07-07 16:46:02 -0400

    fix panel

--- tecno-dynamic/resources/views/layouts/panel.blade.php
+++ tecno-dynamic/resources/views/layouts/panel.blade.php
@@ -19,54 +19,17 @@
 </head>
 
 <body>
-  <!-- Sidenav -->
-  
-  <nav class="navbar navbar-vertical fixed-left navbar-expand-md navbar-light bg-white" id="sidenav-main">
-    <div class="container-fluid">
-      <!-- Toggler -->
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#sidenav-collapse-main" aria-controls="sidenav-main" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <!-- Brand -->
-      <a class="navbar-brand pt-0" href="/">
-        <img src="{{ asset('img/brand/blue.png') }}" class="navbar-brand-img" alt="...">
-      </a>
-      <!-- User -->
-      <ul class="nav align-items-center d-md-none">
-        <li class="nav-item dropdown">
-          <a class="nav-link nav-link-icon" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            <i class="ni ni-bell-55"></i>
-          </a>
-          <div class="dropdown-menu dropdown-menu-arrow dropdown-menu-right" aria-labelledby="navbar-default_dropdown_1">
-            <a class="dropdown-item" href="#">Action</a>
-            <a class="dropdown-item" href="#">Another action</a>
-            <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="#">Something else here</a>
-          </div>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            <div class="media align-items-center">
-              <span class="avatar avatar-sm rounded-circle">
-                <img alt="Image placeholder" src="{{ asset('img/theme/team-1-800x800.jpg') }}">
-              </span>
-            </div>
-          </a>
-          <div class="dropdown-menu dropdown-menu-arrow dropdown-menu-right">
-            <div class=" dropdown-header noti-title">
-              <h6 class="text-overflow m-0">Welcome!</h6>
-            </div>
-            <a href="./examples/profile.html" class="dropdown-item">
-              <i class="ni ni-single-02"></i>
-              <span>My profile</span>
-            </a>
-            <a href="./examples/profile.html" class="dropdown-item">
-              <i class="ni ni-settings-gear-65"></i>
-              <span>Settings</span>
-            </a>
-            <a href="./examples/profile.html" class="dropdown-item">
-              <i class="ni ni-calendar-grid-58"></i>
-              <span>Activity</span>
+    <!-- Sidenav -->
+    <nav class="navbar navbar-vertical fixed-left navbar-expand-md navbar-light bg-white" id="sidenav-main">
+        <div class="container-fluid">
+            <!-- Toggler -->
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#sidenav-collapse-main"
+                aria-controls="sidenav-main" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <!-- Brand -->
+            <a class="navbar-brand pt-0" href="/">
+                <img src="{{ asset('img/brand/blue.png') }}" class="navbar-brand-img" alt="...">
             </a>
             <!-- User -->
             <ul class="nav align-items-center d-md-none">
@@ -203,28 +166,10 @@
 
                 </div>
             </div>
-<<<<<<< HEAD
-=======
-          </li>
-        </ul>
-      </div>
-    </nav>
-    <!-- Header -->
-<<<<<<< HEAD
-    <div class="header bg-gradient-primary pb-6 pt-5 pt-md-3">
-=======
-    <div class="header bg-gradient-primary pb-6 pt-5 pt-md-2">
->>>>>>> fercho
-      <div class="container-fluid">
-        <div class="header-body">
-          <!-- Card stats -->
-          
->>>>>>> alex
         </div>
         <!-- Page content -->
         @yield('content')
 
-<<<<<<< HEAD
 
         <!-- Argon Scripts -->
         <!-- Core -->
@@ -235,21 +180,6 @@
         <script src="{{ asset('vendor/chart.js/dist/Chart.extension.js') }}"></script>
         <!-- Argon JS -->
         <script src="{{ asset('js/argon.js?v=1.0.0') }}"></script>
-=======
-  
-  <!-- Argon Scripts -->
-  <!-- Core -->
-  <script src="{{ asset('vendor/jquery/dist/jquery.min.js') }}"></script>
-  <script src="{{ asset('vendor/bootstrap/dist/js/bootstrap.bundle.min.js') }}"></script>
-  <!-- Optional JS -->
-  <script src="{{ asset('vendor/chart.js/dist/Chart.min.js') }}"></script>
-  <script src="{{ asset('vendor/chart.js/dist/Chart.extension.js') }}"></script>
-  <!-- Argon JS -->
-  <script src="{{ asset('js/argon.js?v=1.0.0') }}"></script>
-
-
-
->>>>>>> fercho
 </body>
 
 </html>
