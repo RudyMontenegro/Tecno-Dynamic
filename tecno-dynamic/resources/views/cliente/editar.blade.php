775f1abc20416d524a5fdc9f876330747599cfc6
kaolio <milanando300@gmail.com>
2021-07-14 15:15:26 -0400

    ed

---
+++ tecno-dynamic/resources/views/cliente/editar.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.panel')
+@section('subtitulo','clientes')
+@section('content')
+<nav aria-label="breadcrumb">
+  <ol class="breadcrumb">
+   
+  </ol>
+</nav>
+
+<div class="box-header ">
+                  <h3 class="text-center">EDITAR CLIENTE</h3>
+            </div></br>
