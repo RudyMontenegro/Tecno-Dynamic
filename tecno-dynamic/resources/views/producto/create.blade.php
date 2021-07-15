4748c419102f6834314d67edd80f3927e975dab9
rolidoss <rudymontenegro4@gmail.com>
2021-07-15 18:13:15 -0400

    mercheadito fer ale

--- tecno-dynamic/resources/views/producto/create.blade.php
+++ tecno-dynamic/resources/views/producto/create.blade.php
@@ -89,147 +89,44 @@
         >
         {!!  $errors->first('precioVentaMayor','<div class="invalid-feedback">:message</div>') !!}
     </div>
-    <div class="col-md-6 mx-auto ">
-        <h2>
-            @include('Mensaje.nota')
-        </h2>
+    <div class="col-5">
+        <label for="precioVentaMenor"class="control-label">{{'Precio Venta Menor'}}</label>
+        <input type="number" step="0.01" class="form-control  {{$errors->has('precioVentaMenor')?'is-invalid':'' }}" name="precioVentaMenor" id="precioVentaMenor" 
+        value="{{ isset($personal->telefono)?$personal->telefono:old('precioVentaMenor')  }}"
+        >
+        {!!  $errors->first('precioVentaMenor','<div class="invalid-feedback">:message</div>') !!}
     </div>
-
-    <form action="{{url('/registrarProducto')}}" class="form" method="post" enctype="multipart/form-data">
-
-        {{ csrf_field()}}
-
-
-        <div class="col-md-11 mx-auto ">
-
-            <div class="row">
-                <div class="col-5">
-
-                    <label for="codigo" class="control-label">{{'Codigo'}}</label>
-                    <input type="text" class="form-control  {{$errors->has('codigo')?'is-invalid':'' }}" name="codigo"
-                        id="codigo" value="{{ isset($personal->nombre)?$personal->nombre:old('codigo') }}">
-                    {!! $errors->first('codigo','<div class="invalid-feedback">:message</div>') !!}
-
-
-                </div>
-                <div class="col-5">
-                    <label for="codigoBarra" class="control-label">{{'Codigo Barra'}}</label>
-                    <input type="text" class="form-control  {{$errors->has('codigoBarra')?'is-invalid':'' }}"
-                        name="codigoBarra" id="codigoBarra"
-                        value="{{ isset($personal->email)?$personal->email:old('codigoBarra')  }}">
-                    {!! $errors->first('codigoBarra','<div class="invalid-feedback">:message</div>') !!}
-                </div>
-
-            </div>
-            <div class="row">
-                <div class="col-5">
-                    <label for="nombre" class="control-label">{{'Nombre'}}</label>
-                    <input type="text" class="form-control  {{$errors->has('nombre')?'is-invalid':'' }}" name="nombre"
-                        id="nombre" value="{{ isset($personal->apellido)?$personal->apellido:old('nombre') }}">
-                    {!! $errors->first('nombre','<div class="invalid-feedback">:message</div>') !!}
-                </div>
-                <div class="col-5">
-                    <label for="categoria" class="control-label">{{'Categoria'}}</label>
-                    <input type="text" class="form-control  {{$errors->has('categoria')?'is-invalid':'' }}"
-                        name="categoria" id="categoria"
-                        value="{{ isset($personal->apellido)?$personal->apellido:old('categoria') }}">
-                    {!! $errors->first('categoria','<div class="invalid-feedback">:message</div>') !!}
-                </div>
-
-
-            </div>
-            <div class="row">
-                <div class="col-5">
-                    <label for="marca" class="control-label">{{'Marca'}}</label>
-                    <input type="text" class="form-control  {{$errors->has('marca')?'is-invalid':'' }}" name="marca"
-                        id="marca" value="{{ isset($personal->codigoSis)?$personal->codigoSis:old('marca') }}">
-                    {!! $errors->first('marca','<div class="invalid-feedback">:message</div>') !!}
-                </div>
-                <div class="col-5">
-                    <label for="precioCosto" class="control-label">{{'Precio Costo'}}</label>
-                    <input type="number" step="0.01"
-                        class="form-control  {{$errors->has('precioCosto')?'is-invalid':'' }}" name="precioCosto"
-                        id="precioCosto"
-                        value="{{ isset($personal->codigoSis)?$personal->codigoSis:old('precioCosto') }}">
-                    {!! $errors->first('precioCosto','<div class="invalid-feedback">:message</div>') !!}
-                </div>
-
-            </div>
-            <div class="row">
-                <div class="col-5">
-                    <label for="precioVentaMayor" class="control-label">{{'Precio Venta Mayor'}}</label>
-                    <input type="number" step="0.01"
-                        class="form-control  {{$errors->has('precioVentaMayor')?'is-invalid':'' }}"
-                        name="precioVentaMayor" id="precioVentaMayor"
-                        value="{{ isset($personal->telefono)?$personal->telefono:old('precioVentaMayor')  }}">
-                    {!! $errors->first('precioVentaMayor','<div class="invalid-feedback">:message</div>') !!}
-                </div>
-                <div class="col-5">
-                    <label for="precioVentaMenor" class="control-label">{{'Precio Venta Menor'}}</label>
-                    <input type="number" step="0.01"
-                        class="form-control  {{$errors->has('precioVentaMenor')?'is-invalid':'' }}"
-                        name="precioVentaMenor" id="precioVentaMenor"
-                        value="{{ isset($personal->telefono)?$personal->telefono:old('precioVentaMenor')  }}">
-                    {!! $errors->first('precioVentaMenor','<div class="invalid-feedback">:message</div>') !!}
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-5">
-                    <label for="cantidad" class="control-label">{{'Cantidad'}}</label>
-                    <input type="text" class="form-control  {{$errors->has('cantidad')?'is-invalid':'' }}"
-                        name="cantidad" id="cantidad"
-                        value="{{ isset($personal->password)?$personal->password:old('cantidad') }}">
-                    {!! $errors->first('cantidad','<div class="invalid-feedback">:message</div>') !!}
-                </div>
-                <div class="col-5">
-                    <label for="unidad" class="control-label">{{'Unidad'}}</label>
-                    <input type="text" class="form-control  {{$errors->has('unidad')?'is-invalid':'' }}" name="unidad"
-                        id="unidad" value="{{ isset($personal->password)?$personal->password:old('unidad') }}">
-                    {!! $errors->first('unidad','<div class="invalid-feedback">:message</div>') !!}
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-5">
-                    <label for="cantidadInicial" class="control-label">{{'Cantidad Inicial'}}</label>
-                    <input type="text" class="form-control  {{$errors->has('cantidadInicial')?'is-invalid':'' }}"
-                        name="cantidadInicial" id="cantidadInicial"
-                        value="{{ isset($personal->password)?$personal->password:old('cantidadInicial') }}">
-                    {!! $errors->first('cantidadInicial','<div class="invalid-feedback">:message</div>') !!}
-                </div>
-                <div class="col-5">
-                    <label for="foto" class="control-label">{{'Foto'}}</label>
-                    <input type="file" class="form-control  {{$errors->has('foto')?'is-invalid':'' }}" name="foto"
-                        id="foto" value="{{ isset($personal->password)?$personal->password:old('foto') }}">
-                    {!! $errors->first('foto','<div class="invalid-feedback">:message</div>') !!}
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-5">
-                    <label for="proveedor">Proveedor</label>
-                    <select name="proveedor" id="proveedor"
-                        class="form-control  {{$errors->has('proveedor')?'is-invalid':'' }}">
-                        <option selected disabled>Elige un Proveedor</option>
-                        @foreach ($proveedor as $proveedor)
-                        <option value="{{$proveedor->id}}">{{$proveedor->nombre_empresa}}</option>
-                        @endforeach
-                    </select>
-                    {!! $errors->first('unidad','<div class="invalid-feedback">:message</div>') !!}
-                </div>
-                <div class="col-5">
-
-                </div>
-
-            </div>
-            <br>
-            <div class="row">
-
-                <div class="col-5">
-                    <a href="{{url('producto')}}" class="btn btn-primary">Regresar</a>
-                </div>
-                <div class="col-5">
-                    <input type="submit" class="btn btn-success float-right" value="Guardar">
-                </div>
-            </div>
+</div>  
+<div class="row">
+    <div class="col-5">
+            <label for="cantidad"class="control-label">{{'Cantidad'}}</label>
+            <input type="text" class="form-control  {{$errors->has('cantidad')?'is-invalid':'' }}" name="cantidad" id="cantidad" 
+            value="{{ isset($personal->password)?$personal->password:old('cantidad') }}"
+            >
+            {!!  $errors->first('cantidad','<div class="invalid-feedback">:message</div>') !!}
+        </div>
+        <div class="col-5">
+            <label for="unidad"class="control-label">{{'Unidad'}}</label>
+            <input type="text" class="form-control  {{$errors->has('unidad')?'is-invalid':'' }}" name="unidad" id="unidad" 
+            value="{{ isset($personal->password)?$personal->password:old('unidad') }}"
+            >
+            {!!  $errors->first('unidad','<div class="invalid-feedback">:message</div>') !!}
+        </div>
+</div> 
+<div class="row">
+    <div class="col-5">
+            <label for="cantidadInicial"class="control-label">{{'Cantidad Inicial'}}</label>
+            <input type="text" class="form-control  {{$errors->has('cantidadInicial')?'is-invalid':'' }}" name="cantidadInicial" id="cantidadInicial" 
+            value="{{ isset($personal->password)?$personal->password:old('cantidadInicial') }}"
+            >
+            {!!  $errors->first('cantidadInicial','<div class="invalid-feedback">:message</div>') !!}
+        </div>
+        <div class="col-5">
+            <label for="foto"class="control-label">{{'Foto'}}</label>
+            <input type="file" class="form-control  {{$errors->has('foto')?'is-invalid':'' }}" name="foto" id="foto" 
+            value="{{ isset($personal->password)?$personal->password:old('foto') }}"
+            >
+            {!!  $errors->first('foto','<div class="invalid-feedback">:message</div>') !!}
         </div>
 </div>  
 <div class="row">
