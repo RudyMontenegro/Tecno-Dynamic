1ce3cd8f0580c289e31eeda73a3d7cc515308edb
rolidoss <rudymontenegro4@gmail.com>
2021-08-03 17:03:16 -0400

    Tabla dinamia (solo frontend) con script añadido

--- tecno-dynamic/resources/views/layouts/panel.blade.php
+++ tecno-dynamic/resources/views/layouts/panel.blade.php
@@ -16,6 +16,9 @@
     <link href="{{ asset('vendor/@fortawesome/fontawesome-free/css/all.min.css') }}" rel="stylesheet">
     <!-- Argon CSS -->
     <link href="{{ asset('css/argon.css?v=1.0.0') }}" rel="stylesheet">
+    
+ 
+
 </head>
 
 <body>
